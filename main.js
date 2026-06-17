1ff2d6e93b06797cd47fa4fe756c61b986703592
Emma Kilcoyne <emmarkilcoyne@gmail.com>
2026-06-17 16:18:31 -0400

    Changed the edit-categories page

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
 
+showOpenFilePicker(){
+        
+    
+    // if there are no files in selected directory
+    alert('No files in directory');
+}
+
 // if there is no file path for the media file folder
-alert('No media file selected');
-// if there are no files in selected directory
-alert('No files in directory');
+    alert('No media file selected');
+
