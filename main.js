5493c7c455aa3d64f92c188b1b3ffc141fe65114
Emma Kilcoyne <emmarkilcoyne@gmail.com>
2026-06-25 11:22:23 -0400

    add ability to select folder

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 
-
-showOpenFilePicker(){
-        
+async function pickFolder(){
+    let [fileHandle] = await window.showDirectoryPicker();
+    console.log(fileHandle);
     
-    // if there are no files in selected directory
-    alert('No files in directory');
 }
+    // if there are no files in selected directory
+    //alert('No files in directory');
+
 
 // if there is no file path for the media file folder
-    alert('No media file selected');
+    //alert('No media file selected');
 
